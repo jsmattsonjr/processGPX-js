406cfe2fef6149e36c2758113085c74083579c0d
Jim Mattson <jsmattsonjr@gmail.com>
2025-08-23 00:53:12 -0700

    Use a set instead of jCount in snapPoints()
    Signed-off-by: Jim Mattson <jsmattsonjr@gmail.com>

--- js/process-gpx.js
+++ js/process-gpx.js
@@ -2095,7 +2095,7 @@ function snapPoints(
 	// note this excludes starting point and end point
 	iLoop: for (let i = 0; i < maxIndex(points) - 1; i += snapStep) {
 		const p1 = points[i];
-		const jCount = [];
+		const visited = new Set();
 
 		let j = i + snapStep;
 		if (j > maxIndex(points)) continue;
@@ -2127,8 +2127,8 @@ function snapPoints(
 		// Keep moving until j comes back into snap range of i
 		jLoop1: while (j <= maxIndex(points)) {
 			// Make sure we don't try the same value twice (moving forward and backward could cause this)
-			jCount[j] = (jCount[j] || 0) + 1;
-			if (jCount[j] > 1) continue iLoop;
+			if (visited.has(j)) continue iLoop;
+			visited.add(j);
 
 			// Looking for j sufficiently close to i and connected with less than a 30% slope
 			// Slope requirement avoids snapping across tight switchbacks or a hypothetical "spiral"
